1c65393ddf5b90c2ad7085417cbe07b02e6d756e
chengchuanqiang <1625320316@qq.com>
2018-04-20 13:53:37 +0800

    配置nginx访问，返回访问服务端口信息

--- src/main/java/com/ccq/controller/UserController.java
+++ src/main/java/com/ccq/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ccq.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,24 +10,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ccq.pojo.User;
 
+import net.sf.json.JSONObject;
+
 @Controller
 public class UserController {
 	
 	@RequestMapping(value = "setUser", method = RequestMethod.GET)
 	@ResponseBody
-	public User setUser(HttpSession session) {
+	public JSONObject setUser(HttpSession session,HttpServletRequest request) {
 		User user = new User();
 		user.setName("ccq");
 		user.setPassword("123");
 		user.setSessionId(session.getId());
 		session.setAttribute("user", user);
-		return user;
+		
+		JSONObject json = JSONObject.fromObject(user);
+		json.put("serverPort", request.getLocalPort());
+		System.out.println(request.getServerPort() + " : " + request.getLocalPort() + " : " + request.getRemotePort() );
+		
+		return json;
 	}
 
 	@RequestMapping(value = "getUser", method = RequestMethod.GET)
 	@ResponseBody
-	public User getUser(HttpSession session) {
+	public JSONObject getUser(HttpSession session,HttpServletRequest request) {
 		User user = (User) session.getAttribute("user");
-		return user;
+		JSONObject json = JSONObject.fromObject(user);
+		json.put("serverPort", request.getLocalPort());
+		System.out.println(request.getServerPort() + " : " + request.getLocalPort() + " : " + request.getRemotePort() );
+		return json;
 	}
 }
